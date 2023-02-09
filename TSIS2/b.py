5de4c93c9551ded5b3725e173eff8c95a47708ba
a d i l e t <sad1let0111@gmail.com>
2023-02-09 14:26:16 +0600

    Delete TSIS2 directory

--- TSIS2/b.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-arr = list(map(int, input().split()))
-max = 0
-for i in range(n):
-    for j in range(i+1, n):
-        if max < arr[i]*arr[j]:
-            max = arr[i]*arr[j]
-print(max)
