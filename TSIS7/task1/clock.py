872d49382a45b299700db05718a5a9d3e388f73d
a d i l e t <sad1let0111@gmail.com>
2023-03-23 22:20:04 +0600

    Add files via upload

---
+++ TSIS7/task1/clock.py
@@ -0,0 +1,36 @@
+import pygame
+import datetime
+
+
+def rot_center(image, angle, x, y):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=image.get_rect(center=(x, y)).center)
+
+    return rotated_image, new_rect
+
+
+pygame.init()
+
+clock_main = pygame.display.set_mode((900, 700))
+h = pygame.image.load("clock_main.png")
+h = pygame.transform.scale(h, clock_main.get_size())
+clock_main.blit(h, (0, 0))
+clock_second = pygame.image.load("clock_second-removebg-preview (1).png")
+cl = clock_main.get_rect().center
+
+clock_minute = pygame.image.load("roma_clock-removebg-preview.png")
+
+while True:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            pygame.quit()
+            exit()
+    clock_main.blit(h, (0, 0))
+    time_now = datetime.datetime.now()
+    seconds_time = time_now.second
+    minutes_time = time_now.minute
+    some_second, pos_second = rot_center(clock_second, 135 - seconds_time * 6, *cl)
+    some_minute, pos_minute = rot_center(clock_minute, 135 - minutes_time * 6, *cl)
+    clock_main.blit(some_second, pos_second)
+    clock_main.blit(some_minute, pos_minute)
+    pygame.display.update()
---
+++ TSIS7/task2/task2.py
@@ -0,0 +1,45 @@
+import pygame
+import os
+import time
+
+pygame.init()
+pygame.display.set_mode()
+
+path = "C:\\Users\\sad1l\\Desktop\\python\\pygame\\musics\\"
+
+list_of_music = os.listdir(path)
+print(list_of_music)
+current = 0
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_p]:
+        pygame.mixer.music.load(path + list_of_music[current])
+        pygame.mixer.music.play(1)
+
+    elif key[pygame.K_SPACE]:
+        if pygame.mixer.music.get_busy():
+            pygame.mixer.music.pause()
+        else:
+            pygame.mixer.music.unpause()
+
+    elif key[pygame.K_n]:
+        if current == len(list_of_music) - 1:
+            current = 0
+        else:
+            current += 1
+        pygame.mixer.music.load(path + list_of_music[current])
+        pygame.mixer.music.play(1)
+
+    elif key[pygame.K_b]:
+        if (current == 0):
+            current = len(list_of_music) - 1
+        else:
+            current -= 1
+        pygame.mixer.music.load(path + list_of_music[current])
+        pygame.mixer.music.play(1)
+    time.sleep(0.1)
